a65fbb7c44484f957292013a654bd38389701b00
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2021-03-17 21:38:15 +0700

    use localStorage

--- AutoProvince.js
+++ AutoProvince.js
@@ -4,28 +4,9 @@ Email : codebee2014@gmail.com
 Website : codebee.co.th
 Company : codebee company limited
 */
-let total;
-function calcArea() {
 
 
-    //microwave
-    let wide = Number(document.querySelector("#wide").value);
-    let long = Number(document.querySelector("#long").value);
-    let wp = Number(document.querySelector("#wp").value);
-
-    total = [((((wide * long) / 2).toFixed(0)) * wp) / 1000];
-
-    document.getElementById("sum").value = total;
-	// var numnum = document.getElementById("sum");
-    // var intairconn = parseInt(num3);
-    //console.log("value of microwave is " + total[0] + " and " + total[1]);
-
-	// document.getElementById("num1").value = numnum;
-	// console.log(total);
-	//callnum(total);
-}
-
-;(function( $ ){
+(function( $ ){
 	$.fn.AutoProvince = function( options ) {
 		var Setting = $.extend( {
 			PROVINCE:		'#province', // select div สำหรับรายชื่อจังหวัด
---
+++ function.js
@@ -0,0 +1,32 @@
+var total = 0;
+var useTotal;
+
+function calcArea() {
+    //microwave
+    let wide = Number(document.querySelector("#wide").value);
+    let long = Number(document.querySelector("#long").value);
+    let wp = Number(document.querySelector("#wp").value);
+
+    total = [((((wide * long) / 2).toFixed(0)) * wp) / 1000];
+
+    document.getElementById("sum").value = total;
+
+    useTotal = document.getElementById("sum").value;
+	console.log("sum is"+document.getElementById("sum").value);
+    console.log("useTotal is "+useTotal);
+
+    //localStorage
+
+    
+}
+// function changeTotal() {
+//     console.log(total);
+//     document.getElementById("testcal").value = document.getElementById("sum").value;
+//     console.log("value of testcal is " + document.getElementById("testcal").value);
+// }
+
+// fucntion showTotal() {
+//     console.log(total);
+//     document.getElementById("testcal2").value = total;
+//     console.log("value of testcal2 is " + document.getElementById("testcal2").value);
+// }
